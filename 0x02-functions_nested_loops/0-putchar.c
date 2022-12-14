26dbddcbd4473340d982dc046e4fbc6425fffc0d
chukwu stanley <chukwuchigozie195@gmail.com>
2022-12-14 14:37:10 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" followed by a new line.
+ *
+ * Description: You are not allowed to include standard libraries.
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+	char _putchar[] = "_putchar";
+
+	int i = 0;
+
+
+
+	while (_puthcar[i] != '\0')
+
+	{
+
+		_putchar(_putchar[i]);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+}
